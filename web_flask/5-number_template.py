4c97571422b40b9319ef7b6352e83cdd4c1c1c54
solp22 <sol.puentereal@gmail.com>
2023-08-14 09:20:36 -0700

    change path of render

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -50,7 +50,7 @@ def number(n):
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    return render_template('templates/5-number.html', n=n)
+    return render_template('5-number.html', n=n)
 
 """host and port that the web app is listening"""
 
