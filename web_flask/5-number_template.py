5fb0009afde8f81c1ec63a5768542cf0996cf667
solp22 <sol.puentereal@gmail.com>
2023-08-14 09:14:48 -0700

    change to uppercase

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -15,17 +15,23 @@ def index():
     return "Hello HBNB!"
 
 """hbnb"""
+
+
 @app.route("/hbnb", strict_slashes=False)
 def hello_world():
     return "HBNB"
 
 """c"""
+
+
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
     new = text.replace("_", " ")
     return f"C {new}"
 
 """python"""
+
+
 @app.route("/python/", defaults={"text": "is_cool"}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_text(text):
@@ -33,15 +39,21 @@ def python_text(text):
     return f"Python {new}"
 
 """number"""
+
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     return f"{n} is a number"
 
 """number template"""
+
+
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     return render_template('templates/5-number.html', n=n)
 
 """host and port that the web app is listening"""
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
