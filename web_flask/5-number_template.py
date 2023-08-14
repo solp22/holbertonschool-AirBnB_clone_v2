1eaa4389e8d7f083584149b66240792ba3eae02d
solp22 <sol.puentereal@gmail.com>
2023-08-14 07:44:29 -0700

    5. Number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""number route module"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+"""routes"""
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+"""hbnb"""
+@app.route("/hbnb", strict_slashes=False)
+def hello_world():
+    return "HBNB"
+
+"""c"""
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    new = text.replace("_", " ")
+    return f"C {new}"
+
+"""python"""
+@app.route("/python/", defaults={"text": "is_cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    new = text.replace("_", " ")
+    return f"Python {new}"
+
+"""number"""
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
+
+"""number template"""
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    return render_template('templates/5-number.html', n=n)
+
+"""host and port that the web app is listening"""
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
