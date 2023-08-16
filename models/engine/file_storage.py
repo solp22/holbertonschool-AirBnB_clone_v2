c9a798e4a6e7a53f79a3fb0135f39812e7db7b63
solp22 <sol.puentereal@gmail.com>
2023-08-16 09:58:20 -0700

    fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,12 +2,24 @@
 """This module defines a class to manage file storage for hbnb clone"""
 import json
 from os.path import exists
-
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
+
+    classes = {
+        'BaseModel': BaseModel, 'User': User, 'Place': Place,
+        'State': State, 'City': City, 'Amenity': Amenity,
+        'Review': Review
+    }
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
@@ -34,22 +46,6 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
-
-    def reload(self):
-        """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-        }
 
     def reload(self):
         """ Deserializes the JSON file to __objects """
