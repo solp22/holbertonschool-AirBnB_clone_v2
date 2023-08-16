ff9da208e1df13cca0e539d0d2f8e46bbc6d1c5b
solp22 <sol.puentereal@gmail.com>
2023-08-16 09:52:09 -0700

    pep8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -50,6 +50,7 @@ class FileStorage:
             'State': State, 'City': City, 'Amenity': Amenity,
             'Review': Review
         }
+
     def reload(self):
         """ Deserializes the JSON file to __objects """
         if exists(self.__file_path):
